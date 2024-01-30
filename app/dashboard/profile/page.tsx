24cfe187c3f900a1c4e15c7a9d186bde8f78bd94
tjfaith <tjbenbiz@gmail.com>
2024-01-31 00:43:11 +0100

    updated

---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import useProfile from './useProfile'
+const Profile = () => {
+    const {} = useProfile();
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
